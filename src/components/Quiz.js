063f29a35abb71a0c2abcabf229ba939aa3471b9
rajesh <merajesh04@gmail.com>
2019-05-26 18:49:43 +0530

    Initial screen design

---
+++ src/components/Quiz.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div className="quiz">
+                <p className="question"> Q. This is the sample question ?</p>
+                <div className="options">
+                    <p>Option 1</p>
+                    <p>Option 2</p>
+                    <p>Option 3</p>
+                    <p>Option 4</p>
+                </div> 
+            </div>
+        );
+    }
+}
+
+export default Quiz;
